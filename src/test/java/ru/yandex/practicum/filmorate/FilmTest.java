81226cf4aee8dc4ccf5744402567723e68d73456
Emilia <elmira.zenkova@yandex.ru>
2025-04-18 01:08:05 +0300

    fz 10 sprint

--- src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
@@ -41,7 +41,7 @@ public class FilmTest extends FilmorateApplicationTests {
         mockMvc.perform(post("/film")
                         .contentType(APPLICATION_JSON)
                         .content("""
-                                {"id":1,"name":"Новое Название 1","description":"Новое Описание 1",
+                                "id":1,"name":"Новое Название 1","description":"Новое Описание 1",
                                 "releaseDate":"2000-07-27","duration":120}
                                 """))
                 .andExpect(status().isCreated());
