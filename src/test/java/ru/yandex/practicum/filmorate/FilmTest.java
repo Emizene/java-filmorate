e31a2833da2a2f9c7f5dc194f31f3de0c8944a6e
Emilia <elmira.zenkova@yandex.ru>
2025-04-18 01:05:28 +0300

    fz 10 sprint

--- src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
@@ -42,7 +42,8 @@ public class FilmTest extends FilmorateApplicationTests {
                         .contentType(APPLICATION_JSON)
                         .content("""
                                 {"id":1,"name":"Новое Название 1","description":"Новое Описание 1",
-                                "releaseDate":"2000-07-27","duration":120}"""))
+                                "releaseDate":"2000-07-27","duration":120}
+                                """))
                 .andExpect(status().isCreated());
         assertEquals(1, filmService.getAllFilms().getBody().size());
     }
@@ -55,8 +56,8 @@ public class FilmTest extends FilmorateApplicationTests {
         mockMvc.perform(patch("/film")
                         .contentType(APPLICATION_JSON)
                         .content("""
-                                	{"id": 1,"name": "new name 1","description": "new description 1",
-                                		"releaseDate": "2000-07-27","duration": 120}
+                                "id": 1,"name": "new name 1","description": "new description 1",
+                                "releaseDate": "2000-07-27","duration": 120}
                                 """))
                 .andExpect(status().isOk());
         Film updateFilm = Objects.requireNonNull(filmService.getAllFilms().getBody()).getFirst();
