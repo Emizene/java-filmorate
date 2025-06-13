93c23e914cfa9f6fb2cdca138778f523350ba571
Emilia <elmira.zenkova@yandex.ru>
2025-06-13 22:34:49 +0300

    add-reviews 13 sprint

--- src/main/java/ru/yandex/practicum/filmorate/dao/ReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewRepository.java
@@ -1,13 +1,9 @@
 package ru.yandex.practicum.filmorate.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Review;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -15,8 +11,6 @@ import java.util.Set;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Optional<Review> findByUserIdAndFilmId(Long userId, Long filmId);
-
-//    Optional<Review> findByUserId(Long userId);
 
     Optional<Set<Review>> findAllByFilmId(Long filmId);
 
